44e2e1c007510835d92af984b1cc75f89d4589be
tea561 <stamenkovicteodora99@gmail.com>
2022-05-17 22:50:48 +0200

    Lab 3 - update

--- LAB2/app/src/main/java/elfak/mosis/myplaces/MainActivity.kt
+++ LAB2/app/src/main/java/elfak/mosis/myplaces/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
-    }
+    }*/
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
